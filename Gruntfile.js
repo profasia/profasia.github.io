d084e7666d4624882d43bf93a5c352fe9a6971e9
Boris Okunskiy <i@savant.pro>
2013-09-17 13:54:47 +0400

    Fixed CDN urls.

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function(grunt) {
           data: {
             pkg: pkg,
             cdn: function(path) {
-              return '/public/' + path;
+              return '/public' + path;
             }
           }
         },
