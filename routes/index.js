3cbcd5289f7a8a647abd7cf8a263e5f10eb9d41e
Boris Okunskiy <i@savant.pro>
2013-09-17 15:28:44 +0400

    Added partial organizations choosing and meta for devices.

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@
 
 var app = require('../app');
 
+app.get('/partials/*.html', function(req, res, next) {
+  if (!req.xhr) return next(new Error(404));
+  res.render('pages/partials/' + req.params[0]);
+});
+
 app.get('/*.html', function(req, res) {
   res.render('pages/' + req.params[0]);
 });
